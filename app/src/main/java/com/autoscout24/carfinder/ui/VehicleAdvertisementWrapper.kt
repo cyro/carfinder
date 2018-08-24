803b66d5eeb819e32b93e27c2482a2749c414a83
christopher_erasmus <christopherkerasmus@gmail.com>
2018-08-24 14:34:56 +0200

    kotlin fixes
    This reverts commit e643b4369f3396f9e0cbd0cf0589da4b1de94b2d.

---
+++ app/src/main/java/com/autoscout24/carfinder/ui/VehicleAdvertisementWrapper.kt
@@ -0,0 +1,25 @@
+package com.autoscout24.carfinder.ui
+
+import com.autoscout24.carfinder.arch.data.database.VehicleEntry
+
+class VehicleAdvertisementWrapper {
+
+    var vehicleEntry: VehicleEntry? = null
+    //Could change this later to fetch custom Advertisements
+    var advertisement: String? = null
+
+    fun isVehicle() = vehicleEntry != null
+    fun isAdvertisement() = advertisement != null
+
+    override fun toString(): String {
+        return if (vehicleEntry != null) "Vehicle" else "Advertisement"
+    }
+
+    constructor(vehicleEntry: VehicleEntry) {
+        this.vehicleEntry = vehicleEntry
+    }
+
+    constructor(advertisement: String) {
+        this.advertisement = advertisement
+    }
+} 
